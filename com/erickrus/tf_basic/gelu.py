059ba175bd7ca4471b829f421a521402a06c56e2
admin <admin@admindeMacBook-Air.local>
2019-03-29 14:30:30 +0800

    updated

--- com/erickrus/tf_basic/gelu.py
+++ com/erickrus/tf_basic/gelu.py
@@ -5,7 +5,7 @@ import numpy as np
 # https://www.programcreek.com/python/example/90479/tensorflow.erf
 
 def gelu(x):
-  return x * tf.erfc(-x / tf.sqrt(2.)) / 2.
+  return tf.multiply(x, tf.erfc(-x / tf.sqrt(2.)) / 2.)
 
 # basically, we use fast_gelu
 def fast_gelu(x):
