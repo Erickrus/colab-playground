0a7b6ae5bf07a83361905d81eb0fcc56686de236
Erickrus <hyinghao@hotmail.com>
2019-03-30 15:35:56 +0800

    updated

--- com/erickrus/ctr/top_list.py
+++ com/erickrus/ctr/top_list.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 
 class TopList:
+
   def gravity_score(self, post, currTime, gravity=1.8):
     """
     post.clickNum:            P表示物品得票数；
@@ -10,13 +11,16 @@ class TopList:
     """
     return (post.clickNum - 1.0) / (currTime - post.postTime + 2.0) ** gravity
 
+
   # http://www.ruanyifeng.com/blog/2012/03/ranking_algorithm_newton_s_law_of_cooling.html
   def exponential_decay_score(self, post, currTime, alpha=0.192):
     """
+    函数基于 Newton's Law of cooling
     本期得分 = 上一期得分 x exp(-(冷却系数) x 间隔的小时数) 
     冷却系数 是一个你自己决定的值。如果假定一篇新文章的初始分数是100分，24小时之后"冷却"为1分，那么可以计算得到"冷却系数"约等于0.192。如果你想放慢"热文排名"的更新率，"冷却系数"就取一个较小的值，否则就取一个较大的值。
     """
     return post.clickNum * math.exp(- alpha * (currTime - post.postTime))
+
 
   # the script reference following site
   # http://www.ruanyifeng.com/blog/2012/03/ranking_algorithm_bayesian_average.html
@@ -56,5 +60,8 @@ if __name__ == "__main__":
   print(tList.gravity_score(p, 20))
   print(tList.gravity_score(p, 21))
 
-  print(tList.exponential_decay(p, 20))
-  print(tList.exponential_decay(p, 21))
+  print(tList.exponential_decay_score(p, 20))
+  print(tList.exponential_decay_score(p, 21))
+
+  ba = BayesianAverage()
+  print(ba.average(np.array(list(range(20))),100,5))
