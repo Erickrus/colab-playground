e646f2a3e56a6e866a7fd7c73a5f642f9450357f
Erickrus <hyinghao@hotmail.com>
2019-03-30 15:32:28 +0800

    updated

--- com/erickrus/ctr/top_list.py
+++ com/erickrus/ctr/top_list.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 class TopList:
   def gravity_score(self, post, currTime, gravity=1.8):
@@ -16,6 +17,37 @@ class TopList:
     冷却系数 是一个你自己决定的值。如果假定一篇新文章的初始分数是100分，24小时之后"冷却"为1分，那么可以计算得到"冷却系数"约等于0.192。如果你想放慢"热文排名"的更新率，"冷却系数"就取一个较小的值，否则就取一个较大的值。
     """
     return post.clickNum * math.exp(- alpha * (currTime - post.postTime))
+
+  # the script reference following site
+  # http://www.ruanyifeng.com/blog/2012/03/ranking_algorithm_bayesian_average.html
+  def bayesian_average(self, x, C, m):
+    """
+    贝叶斯平均主要解决的是如果被评选的项目数据较少的问题
+    用一个先验概率放在里面作为辅助，得到一个调整后的(加权)值
+
+    先估计一个值，然后不断用新的信息修正，使得它越来越接近正确的值，
+    如果投票人数少，总分接近全局平均分，
+    如果投票人数多，总分接近项目平均分
+
+    n是该项目的投票人数；
+    x是该项目的每张选票的值；
+    m是平均投票得分；
+    C是平均投票人数
+
+    此处的公式是一种一般形式的贝叶斯平均公式，之前文中提到的imdb分值也是这个公式的一种变型
+    m（总体平均分）是"先验概率"，每一次新的投票都是一个调整因子，使总体平均分不断向该项目的真实投票结果靠近。
+    投票人数越多，该项目的"贝叶斯平均"就越接近算术平均，对排名的影响就越小。
+    因此，这种方法可以给一些投票人数较少的项目，以相对公平的排名。
+
+    C*m    /        C = 历史上所有得分 / 历史上所有投票次数
+    sum(x) / count(x) = 当前所有得分   / 当前所有投票次数
+
+    缺点，主要问题是它假设用户的投票是正态分布
+    """
+    xBar = (C*m + np.sum(x)) / (C + x.size)
+    return xBar
+
+
 
 if __name__ == "__main__":
   from com.erickrus.ctr.post import Post
